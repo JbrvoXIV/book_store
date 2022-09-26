2483c1ac5b7cb92abbe41709741c579874f3b5b0
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2022-09-26 10:03:21 -0400

    Linked Schemas, Created Git request

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 A group project for CEN4010
-=======
-My copy of the source code for a book store api
-im in charge of implementing the shopping cart
->>>>>>> 2f5378bc664bc3f34545e33cc8ef94663281fac9
--- server/models/shoppingCarts.model.js
+++ server/models/shoppingCarts.model.js
@@ -1,117 +1,14 @@
 const mongoose = require('mongoose');
-
-// MORE INFO WILL BE NEEDED TO ADD
-const booksInCart = new mongoose.Schema ({
-    title: {type:String},
-    author: {type:String},
-    year: {type:String},
-});
-
-const adressInfo = new mongoose.Schema({
-    city: {type:String},
-    state: {type:String},
-    street: {type:String},
-    zip: {type:String},
-});
-
-const userInfo = new mongoose.Schema ({
-    name: {type:String},
-    user_name: {type:String},
-    email:{type:String},
-    adress: {type:adressInfo},
-});
+const Schema = mongoose.Schema;
 
 const shoppingCartSchema = new mongoose.Schema({
-    shoppingCart:[booksInCart],
-    user:{
-        type:userInfo
-    },
-});
-
-
-/*
-const moviesSchema = new mongoose.Schema({
-    plot:{
-        type:String
-    } ,
-    genres:[{
-        type:String
-    }],
-    runtime:{
-        type:Number
-    },
-    cast:[{
-        type: String
-    }],
-    num_mflix_comments:{
-        type: Number
-    },
-    title:{
-        type: String
-    },
-    countries:[{
-        type: String
-    }],
-    released:{
-        type: Date
-    },
-    directors:[{
-        type: String
-    }],
-    rated:{
-        type: String
-    },
-    awards:{
-        wins:{
-            type: Number
-        },
-        nominations: {
-            type: Number
-        },
-        text: {
-            type: String
-        }
-    },
-    lastupdated:{
-        type: String
-    },
-    year: {
-        type: Number
-    },
-    imdb:{
-        rating: {
-            type: Number
-        },
-        votes:{
-            type: Number
-        },
-        id:{
-            type: Number
-        }
-    },
-    type: {
-        type: String
-    },
-    tomatoes:{
-        viewer:{
-            rating: {
-                type: Number
-            },
-            numReviews:{
-                type: Number
-            },
-            meter:{
-                type: Number
-            }
-        },
-        lastupdated: {
-            type:Date
-        }
+    shoppingCart:[{type: Schema.ObjectId, ref: 'Books'},],
+    user: {
+        type: Schema.ObjectId,
+        ref: 'Users'
     }
-
-
 });
 
-*/
+const ShoppingCart = mongoose.model('ShoppingCart', shoppingCartSchema, "shoppingcarts");
 
-module.exports = mongoose.model('ShoppingCarts', shoppingCartSchema);
+module.exports = ShoppingCart;
