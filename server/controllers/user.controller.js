57cb71f70b21ae736222f94b86f2fe5982f8a1f1
Jordan Bravo <jbravo0423@gmail.com>
2022-10-03 00:20:20 -0400

    rebased from main

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,7 +4,11 @@ const { User } = require('../models/user.model.js');
 const getUserByUserNameController = async (req, res) => {
     try {
         const {userName} = req.query;
+<<<<<<< HEAD
         const user = await User.find({ user_name: userName })
+=======
+        const user = await User.find({ user_name: userName });
+>>>>>>> ec88cfc (updated book schema)
         return res.status(200).json(user);
     } catch(e) {
         console.log(e);
