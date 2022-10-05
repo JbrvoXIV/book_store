5c871c8e05e5855ef70c3a6d34af43d28cf1f6da
HerbertCabrera <hcabr025@fiu.edu>
2022-10-05 00:01:07 -0400

    Book details code implementation.
    -Implemented controller methods to do get calls filtering bi ISBN and
    author's name the books.
    -Expanded book model to have  more properties.

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 const server = 'https://localhost:';
 const userRouter = require('./routes/user.route.js');
 const shoppingCartRouter = require('./routes/shoppingCarts.route.js');
+<<<<<<< HEAD
 const bookBrowsingRouter = require('./routes/bookBrowsing.routes.js');
 const wishListRouter = require('./routes/wishList.route.js');
 
@@ -16,6 +17,9 @@ const bookRouter = require('./routes/bookDetails.route.js');
 const bookRating = require('./routes/ratings.route.js');
 =======
 >>>>>>> b013758 (rebased book_details with main part2)
+=======
+const bookRouter = require('./routes/bookDetails.route.js');
+>>>>>>> c17769d (Book details code implementation.)
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true });
@@ -26,9 +30,12 @@ app.use(express.json());
 app.use(cors());
 app.use('/user', userRouter);
 app.use('/shoppingCart', shoppingCartRouter);
+<<<<<<< HEAD
 app.use('/bookBrowsing', bookBrowsingRouter);
 app.use('/wishList', wishListRouter)
 
+=======
+>>>>>>> c17769d (Book details code implementation.)
 app.use('/book', bookRouter);
 // this gets the server up and running on the port 3000
 app.use('/ratings', bookRating);
