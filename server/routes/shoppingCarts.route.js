ae7f865ebc9ca44e5c4630e43f41b83ef93887b9
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2022-10-11 22:44:11 -0400

    implemented post request

--- server/routes/shoppingCarts.route.js
+++ server/routes/shoppingCarts.route.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
 //const { getAllMoviesController, getMovieController, getMovieByIDController, getMovieByRatingController } = require('../controllers/shoppingCarts.Controller.js');
-const { getAllShoppingCarts } = require('../controllers/shoppingCarts.Controller.js');
+const { getAllShoppingCarts, createCart} = require('../controllers/shoppingCarts.Controller.js');
 
 router.get('/all', getAllShoppingCarts);
-
+router.post('/createCart', createCart);
 
 
 module.exports = router;
