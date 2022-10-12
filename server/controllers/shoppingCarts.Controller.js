d5168912c1dc2d3a975ada35b1a1badcf0441738
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2022-10-11 22:44:11 -0400

    implemented post request

--- server/controllers/shoppingCarts.Controller.js
+++ server/controllers/shoppingCarts.Controller.js
@@ -1,4 +1,6 @@
 const shoppingCarts = require('../models/shoppingCarts.model.js');      
+const {Book} = require('../models/bookBrowsing.model.js');
+const {User} = require('../models/user.model.js');
 
 // get shoppingCart information
 const getAllShoppingCarts = async (req, res) => {
@@ -11,5 +13,39 @@ const getAllShoppingCarts = async (req, res) => {
     }  
 };
 
+const createCart = async (req, res) => {
 
-module.exports = {getAllShoppingCarts};
+    try{
+
+        const book = await Book.find({title: req.body.title});
+        const user = await User.find({user_name: req.body.user_name});
+        
+        if(book.length < 1) {
+            throw "Cannot create shopping cart, book doesn't exist!";
+        }
+
+        if(user.length < 1) {
+            throw "User doesn't exist";
+        }
+
+        const data = new shoppingCarts ({
+            shoppingCart:[{book: book[0]._id}],
+            user: user[0]._id
+        })
+        
+        await data.save();
+
+        return res.status(200).json(data);
+        
+        
+    } catch(e) {
+        console.log(e);
+        return res.status(404).json({ sucess: false, message: e});
+    }
+
+
+
+
+}
+
+module.exports = {getAllShoppingCarts, createCart};
