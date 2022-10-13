b07854ca1ab18e32f8f63b77946795a9419e3d5c
Robert Calistri <robertcalistri@Roberts-Air-2.lan>
2022-10-13 12:41:02 -0400

    Updated controllers, models, and routes to allow user to create a book rating

--- server/models/ratings.model.js
+++ server/models/ratings.model.js
@@ -2,13 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ratingSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    year: String,
-    rating: Number,
-    comment: String,
-    username: String
-
+    title: {
+        type: String,
+        required: [true, "Add book title"]
+    },
+    author: {
+        type: String,
+        required: [true, "Add book author"]
+    },
+    year: {
+        type: String,
+        required: false
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    comment: {
+        type: String,
+        required: false
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Number,
+        required: true
+    }
 }, {collection: 'ratings'});
 
 const book_ratings = mongoose.model('ratings', ratingSchema);
