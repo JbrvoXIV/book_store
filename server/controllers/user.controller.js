4944caa413b75dde33b4c7652ef440eb787e2622
JbrvoXIV <jbravo0423@gmail.com>
2022-10-15 11:57:08 -0400

    implemented POST request for user creation
    -created controller to handle POST request
    -updated user.model to require uniqueness on email, user_name
    -removed uniqueness from address object
    -created a new route for the POST request

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,7 +4,7 @@ const { User } = require('../models/user.model.js');
 const getUserByUserNameController = async (req, res) => {
     try {
         const {userName} = req.query;
-        const user = await User.find({ user_name: userName })
+        const user = await User.find({ user_name: userName });
         return res.status(200).json(user);
     } catch(e) {
         console.log(e);
@@ -12,4 +12,27 @@ const getUserByUserNameController = async (req, res) => {
     }
 };
 
-module.exports = { getUserByUserNameController };
+const createUserController = async (req, res) => {
+    try {
+        const user = req.body;
+        const newUser = new User({
+            name: user.name,
+            user_name: user.user_name,
+            email: user.email,
+            address: {
+                city: user.city,
+                state: user.state,
+                street: user.street,
+                zip: user.zip
+            }
+        });
+        await newUser.save();
+        res.status(201).json({ 
+            status: 201, message: "User successfully created", user: newUser
+        });
+    } catch(e) {
+        res.status(401).json({ status: 401, message: e.message })
+    }
+};
+
+module.exports = { getUserByUserNameController, createUserController };
