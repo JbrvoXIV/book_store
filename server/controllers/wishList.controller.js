4e10a5d082f606bdd37e5d1f37609f29dc234d1d
alejandrocc27 <alejandrocc27@yahoo.com>
2022-10-17 00:47:37 -0400

    Rebased with main

--- server/controllers/wishList.controller.js
+++ server/controllers/wishList.controller.js
@@ -1,19 +1,43 @@
 const {wishList} = require('../models/wishList.model.js');
+const {User} = require('../models/user.model.js');
+const {Book} = require('../models/bookBrowsing.model.js');
 
 const getWishListController = async (req, res) => {
     try{
         const {userName} = req.query
-        console.log(userName)
         
         const list = await wishList.find({
             user: userName
 
         }).populate('books')
-        console.log(list)
+
         return res.status(200).json(list)
     }catch(e) {
         return res.status(404).json({ status: 'failed', message: 'wish list not found in database' });
     }  
 };
 
-module.exports = {getWishListController}
+const createWishListController = async (req, res) => {
+    try {
+        const bookFinder = await Book.find({
+            title: req.body.title
+        })
+
+        const userFinder = await User.find({
+            user_name: req.body.user_name
+        })
+        
+
+        const newList = new wishList({
+            name_of_list: req.body.name_of_list,
+            user: userFinder[0]._id,
+            books:[bookFinder[0]._id]
+        });
+        await newList.save();
+        res.status(201).json({status: 201, message:"Wish list created", wishList:newList});
+    }catch(e){
+        res.status(401).json({ status: 401, message: e.message})
+    }
+}
+
+module.exports = {getWishListController, createWishListController}
