78f51fa044208d3cf2da4965508efe7aeb6c929e
HerbertCabrera <hcabrera@welsfoundation.org>
2022-10-16 22:55:53 -0400

    solving conflicts

--- server/index.js
+++ server/index.js
@@ -8,18 +8,11 @@ const PORT = 3000;
 const server = 'https://localhost:';
 const userRouter = require('./routes/user.route.js');
 const shoppingCartRouter = require('./routes/shoppingCarts.route.js');
-<<<<<<< HEAD
 const bookBrowsingRouter = require('./routes/bookBrowsing.routes.js');
 const wishListRouter = require('./routes/wishList.route.js');
 
 const bookRouter = require('./routes/bookDetails.route.js');
-<<<<<<< HEAD
 const bookRating = require('./routes/ratings.route.js');
-=======
->>>>>>> b013758 (rebased book_details with main part2)
-=======
-const bookRouter = require('./routes/bookDetails.route.js');
->>>>>>> c17769d (Book details code implementation.)
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true });
@@ -30,12 +23,9 @@ app.use(express.json());
 app.use(cors());
 app.use('/user', userRouter);
 app.use('/shoppingCart', shoppingCartRouter);
-<<<<<<< HEAD
 app.use('/bookBrowsing', bookBrowsingRouter);
 app.use('/wishList', wishListRouter)
 
-=======
->>>>>>> c17769d (Book details code implementation.)
 app.use('/book', bookRouter);
 // this gets the server up and running on the port 3000
 app.use('/ratings', bookRating);
