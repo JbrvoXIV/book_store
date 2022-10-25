a725a0f0ad722f14b4a206a373316d6f91c36885
Jordan Bravo <jbravo0423@gmail.com>
2022-10-25 00:03:49 -0400

    Implemented PATCH request for user update
    - Implemented PATCH request that searches for user by user_name
    - Added updateUserController for handling of user update request
    - Added '/update/' route for updateUserController

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -31,8 +31,25 @@ const createUserController = async (req, res) => {
             status: 201, message: "User successfully created", user: newUser
         });
     } catch(e) {
-        res.status(401).json({ status: 401, message: e.message })
+        res.status(401).json({ status: 401, message: e.message });
     }
 };
 
-module.exports = { getUserByUserNameController, createUserController };
+const updateUserController = async (req, res) => {
+    let filter = req.query;
+    let update = req.body;
+    try {
+        if(update.hasOwnProperty('email')) // if user wants to update email, return error
+            return res.status(400).json({ status: 400, message: 'Email cannot be updated' });
+        const updatedUser = await User.findOneAndUpdate(filter, update, { new: true }); // query and update all-in-one, throwns validator errors
+        if(!updatedUser) // check if query returns null, throw error
+            throw 'The user you are trying to update does not exist';
+        await updatedUser.save();
+        return res.status(200).json({ status: 200, message: 'User successfully updated', user: updatedUser });
+    } catch(e) {
+        const msg = e.message ? e.message : e; // if error is thrown by execution, e.message, else, custom message e
+        return res.status(404).json({ status: 404, message: msg});
+    }
+}
+
+module.exports = { getUserByUserNameController, createUserController, updateUserController };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 
-const { getUserByUserNameController, createUserController } = require('../controllers/user.controller.js');
+const { 
+    getUserByUserNameController,
+    createUserController,
+    updateUserController
+} = require('../controllers/user.controller.js');
 
 router.get('/', getUserByUserNameController);
 router.post('/create', createUserController);
+router.patch('/update/', updateUserController);
 
 module.exports = router;
