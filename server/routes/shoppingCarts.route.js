14247ffab501a9a383d1ddc28fd9815258a65b05
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2022-10-31 12:02:54 -0700

    working on adding books to cart

--- server/routes/shoppingCarts.route.js
+++ server/routes/shoppingCarts.route.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 
 //const { getAllMoviesController, getMovieController, getMovieByIDController, getMovieByRatingController } = require('../controllers/shoppingCarts.Controller.js');
-const { getAllShoppingCarts, createCart} = require('../controllers/shoppingCarts.Controller.js');
+const {getAllShoppingCarts, createCart, addBook} = require('../controllers/shoppingCarts.Controller.js');
 
 router.get('/all', getAllShoppingCarts);
 router.post('/createCart', createCart);
+router.patch('/addBook', addBook);
 
 
 module.exports = router;
