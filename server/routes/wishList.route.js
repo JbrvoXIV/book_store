d5e3307df08d2dacd572f1ceac8a64908ec692a4
alejandrocc27 <alejandrocc27@yahoo.com>
2022-10-31 14:10:55 -0400

    incomplete code

--- server/routes/wishList.route.js
+++ server/routes/wishList.route.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
-const { getWishListController, createWishListController } = require('../controllers/wishList.controller.js');
+const { getWishListController, createWishListController, updateWishListController } = require('../controllers/wishList.controller.js');
 
 router.get('/', getWishListController);
-router.post('/create',createWishListController)
+router.post('/create',createWishListController);
+router.patch('/update', updateWishListController);
 
 module.exports = router;
