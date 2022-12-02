04758fb023aaf4279dfef3766d3fb995a1742f9c
alejandrocc27 <alejandrocc27@yahoo.com>
2022-12-01 23:56:36 -0500

    Created a controller to remove book

--- server/routes/wishList.route.js
+++ server/routes/wishList.route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
-const { getWishListController, createWishListController, updateWishListController } = require('../controllers/wishList.controller.js');
+const { getWishListController, createWishListController, updateWishListController, removeBookController } = require('../controllers/wishList.controller.js');
 
 router.get('/', getWishListController);
 router.post('/create',createWishListController);
 router.patch('/update', updateWishListController);
+router.patch('/remove', removeBookController)
 
 module.exports = router;
